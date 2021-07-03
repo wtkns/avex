193ac864e12591f5539c61d4d7dad964b5c76f26
james <jms@wtkns.com>
2021-07-02 21:24:08 -0700

    added system.cpp and plain CMakeLists.txt

--- projects/system/system.cpp
+++ projects/system/system.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello World!";
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+//    system("jackd -dalsa -dhw:U192k");
+//    system("scsynth -u 57120");
     return 0;
 }
