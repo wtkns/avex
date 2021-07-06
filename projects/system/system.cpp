07a416b9b7fc3526f55b1d6d13e7f0ab0eed469d
james <jms@wtkns.com>
2021-07-05 20:29:33 -0700

    added boost and googletest library dependency

--- projects/system/system.cpp
+++ projects/system/system.cpp
@@ -1,8 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
 int main()
 {
-//    system("jackd -dalsa -dhw:U192k");
-//    system("scsynth -u 57120");
-    return 0;
+    using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    std::for_each(
+            in(std::cin), in(), std::cout << (_1 * 3) << " " );
 }
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//#include <boost/process.hpp>
+//
+//using namespace boost::process;
+//
+//int main()
+//{
+//    ipstream pipe_stream;
+//    child c("gcc --version", std_out > pipe_stream);
+//
+//    std::string line;
+//
+//    while (pipe_stream && std::getline(pipe_stream, line) && !line.empty())
+//        std::cerr << line << std::endl;
+//
+//
+////    //    system("jackd -dalsa -dhw:U192k");
+////    system("scsynth -u 57130");
+//////    system("./sendosc 127.0.0.1 57130 /dumpOSC i 1");
+//
+//    c.wait();
+//}
